e7058c0b6cbd07c5a84767ed6f3b7a16f6abf93e
Hoffmann77 <36714809+Hoffmann77@users.noreply.github.com>
2024-10-21 20:53:54 +0200

    wip: fix tests.

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -78,7 +78,7 @@ async def get_gateway(fixture_name):
         print(f"endpoint: {endpoint} update required {endpoint.update_required}")
 
         return_value = await gen_response(fixture_name, endpoint.path)
-        print(f"return value: {return_value}")
+        print(f"return value: {return_value.text}")
         respx.get(f"/{endpoint.path}").mock(return_value=return_value)
 
     # Update two times to verify the gateway does not drop the required
